6f8df367d3ddb99bca662dee086c6045626cd1ae
DavidTobar456 <88524503+DavidTobar456@users.noreply.github.com>
2024-07-04 08:52:38 -0600

    Added all register parameters and bits
    Added PNG_LATCH, DITHER, DISCHG, SR, BBFSW, ILIM for MPQ4210 and MPQ4214 devices to parameter list. Updated function definitions

--- MPQ4210.h
+++ MPQ4210.h
@@ -5,9 +5,17 @@ extern void I2C_WriteRegByte(uint8_t SlaveAddress, uint8_t RegAddress, uint8_t B
 extern uint8_t I2C_ReadRegByte(uint8_t SlaveAddress, uint8_t RegAddress);                   //Read a byte from the device register via I2C
 extern void SoftwareDelay(uint8_t ms);                                                      //Software delay in milliseconds
 
-//MPQ4210 register address definition
-#define MPQ4210_ADDR1                   0x50
-#define MPQ4210_ADDR2                   0x50
+//MPQ4210 address definition
+#define MPQ4210_ADDR1                   0x60
+#define MPQ4210_ADDR2                   0x66
+
+//MPQ4214 address definition
+#define MPQ4210_ADDR1                   0x60
+#define MPQ4210_ADDR2                   0x62
+#define MPQ4210_ADDR3                   0x64
+#define MPQ4210_ADDR4                   0x66
+
+//MPQ421x register definition
 #define MPQREG_REF_LSB                  0x00
 #define MPQREG_REF_MSB                  0x01
 #define MPQREG_CONTROL1                 0x02
@@ -17,7 +25,7 @@ extern void SoftwareDelay(uint8_t ms);                                          
 #define MPQREG_INT_MASK                 0x06
 
 /*
-* MPQ4210 Hardware initialization structure parameters
+* MPQ4210 and MPQ4214 Hardware initialization structure parameters
 * @{
 */
 
@@ -35,6 +43,28 @@ extern void SoftwareDelay(uint8_t ms);                                          
 #define MPQ_CONTROL1_ENPWR_DIS          0x00
 #define MPQ_CONTROL1_ENPWR_EN           0x01
 
+// MPQ_CONTROL1_PNG_LATCH parameters definition
+#define MPQ_CONTROL1_PNG_LATCH_MASK     0xF7
+#define MPQ_CONTROL1_PNG_LATCH_CLR      0x00
+#define MPQ_CONTROL1_PNG_LATCH_SET      0x08
+
+// MPQ_CONTROL1_DITHER parameters definition
+#define MPQ_CONTROL1_DITHER_MASK        0xEF
+#define MPQ_CONTROL1_DITHER_DIS         0x00
+#define MPQ_CONTROL1_DITHER_EN          0x10
+
+// MPQ_CONTROL1_DISCHG parameters definition
+#define MPQ_CONTROL1_DISCHG_MASK        0xDF
+#define MPQ_CONTROL1_DISCHG_OFF         0x00
+#define MPQ_CONTROL1_DISCHG_ON          0x20
+
+// MPQ_CONTROL1_SR parameters definition
+#define MPQ_CONTROL1_SR_MASK            0x3F
+#define MPQ_CONTROL1_SR_38mV_ms         0x00
+#define MPQ_CONTROL1_SR_50mV_ms         0x40
+#define MPQ_CONTROL1_SR_75mV_ms         0x80
+#define MPQ_CONTROL1_SR_150mV_ms        0xC0
+
 // MPQ_CONTROL2_FSW parateters definition
 #define MPQ_CONTROL2_FSW_MASK           0x3F
 #define MPQ_CONTROL2_FSW_200khz         0x00
@@ -42,69 +72,64 @@ extern void SoftwareDelay(uint8_t ms);                                          
 #define MPQ_CONTROL2_FSW_400khz         0x80
 #define MPQ_CONTROL2_FSW_600khz         0xC0
 
+// MPQ_CONTROL2_BBFSW parateters definition
+#define MPQ_CONTROL2_BBFSW_MASK         0xEF
+#define MPQ_CONTROL2_BBFSW_OFF          0x00
+#define MPQ_CONTROL2_BBFSW_ON           0x10
+
 // MPQ_CONTROL2_OCP_MODE parameters definition
 #define MPQ_CONTROL2_OCP_MODE_MASK      0xF3
 #define MPQ_CONTROL2_OCP_MODE_NONE      0x00
 #define MPQ_CONTROL2_OCP_MODE_HICCUP    0x04
 #define MPQ_CONTROL2_OCP_MODE_LATCH     0x08
 
-// MPQ_CONTROL2_OCP_MODE parameters definition
+// MPQ_CONTROL2_OVP_MODE parameters definition
 #define MPQ_CONTROL2_OVP_MODE_MASK      0xFC
 #define MPQ_CONTROL2_OVP_MODE_NONE      0x00
 #define MPQ_CONTROL2_OVP_MODE_HICCUP    0x04
 #define MPQ_CONTROL2_OVP_MODE_LATCH     0x08
 
-// MPQ_ILIM Parameters definition
-#define MPQ_ILIM_27_9mV                 0x00
-#define MPQ_ILIM_33_3mV                 0x01
-#define MPQ_ILIM_39_3mV                 0x02
-#define MPQ_ILIM_45_1mV                 0x03
-#define MPQ_ILIM_51_2mV                 0x04
-#define MPQ_ILIM_56_8mV                 0x05
-#define MPQ_ILIM_62_8mV                 0x06
-#define MPQ_ILIM_68_7mV                 0x07
+// MPQ_ILIM Parameters definition for MPQ4210 devices
+#define MPQ4210_ILIM_MASK               0xF8
+#define MPQ4210_ILIM_27_9mV             0x00
+#define MPQ4210_ILIM_33_3mV             0x01
+#define MPQ4210_ILIM_39_3mV             0x02
+#define MPQ4210_ILIM_45_1mV             0x03
+#define MPQ4210_ILIM_51_2mV             0x04
+#define MPQ4210_ILIM_56_8mV             0x05
+#define MPQ4210_ILIM_62_8mV             0x06
+#define MPQ4210_ILIM_68_7mV             0x07
+
+// MPQ_ILIM Parameters definition for MPQ4214 devices
+#define MPQ4214_ILIM_MASK               0xF8
+#define MPQ4214_ILIM_26mV               0x00
+#define MPQ4214_ILIM_32mV               0x01
+#define MPQ4214_ILIM_38mV               0x02
+#define MPQ4214_ILIM_45mV               0x03
+#define MPQ4214_ILIM_50mV               0x04
+#define MPQ4214_ILIM_56mV               0x05
+#define MPQ4214_ILIM_62mV               0x06
+#define MPQ4214_ILIM_68mV               0x07
 
 /*
 * MPQ4210 hardware configuration functions
 */
 
-// Function to set the VREF registers on the MPQ4210 device
-// when MPQ4210's address is 0x64
-void MPQ_SetVoltageReference1(uint16_t Vref);
+// Function to set the VREF registers on the MPQ421x devices
+void MPQ_SetVoltageReference1(uint8_t deviceAddress,uint16_t Vref);
 
-// Function to set the VREF registers on the MPQ4210 device
-// when MPQ4210's address is 0x66
-void MPQ_SetVoltageReference2(uint16_t Vref);
+// Function to set the switching frequency on the MPQ421x device
+void MPQ_SetSwitchingFrequency(uint8_t deviceAddress,uint8_t Fsw);
 
-// Function to set the switching frequency on the MPQ4210 device
-// when MPQ4210's address is 0x64
-void MPQ_SetSwitchingFrequency1(uint8_t Fsw);
+// Function to set OCP Mode on the MPQ421x device
+void MPQ_setOCPMode(uint8_t deviceAddress,uint8_t OCPMode);
 
-// Function to set the switching frequency on the MPQ4210 device
-// when MPQ4210's address is 0x66
-void MPQ_SetSwitchingFrequency2(uint8_t Fsw);
+// Function to set OVP Mode on the MPQ421x device
+void MPQ_setOVPMode(uint8_t deviceAddress,uint8_t OVPMode);
 
-// Function to set OVP Mode on the MPQ4210 device
-// when MPQ4210's address is 0x64
-void MPQ_setOCPMode1(uint8_t OVPMode);
+// Function to set ILIM thresthold on MPQ4210 devices
+void MPQ4210_setILIM(uint8_t deviceAddress,uint8_t ILIMthreshold);
 
-// Function to set OVP Mode on the MPQ4210 device
-// when MPQ4210's address is 0x66
-void MPQ_setOCPMode2(uint8_t OVPMode);
-
-// Function to set OVP Mode on the MPQ4210 device
-// when MPQ4210's address is 0x64
-void MPQ_setOVPMode2(uint8_t OVPMode);
-
-// Function to set OVP Mode on the MPQ4210 device
-// when MPQ4210's address is 0x66
-void MPQ_setOVPMode2(uint8_t OVPMode);
-
-// Function to set ILIM thresthold
-// when MPQ4210's address is 0x64
-void MPQ_setILIM1(uint8_t ILIMthreshold);
-
-// Function to set ILIM thresthold
-// when MPQ4210's address is 0x66
-void MPQ_setILIM2(uint8_t ILIMthreshold);
+// Function to set ILIM thresthold on MPQ4214 devices
+void MPQ4214_setILIM(uint8_t deviceAddress,uint8_t ILIMthreshold);
 
